83e1df586d95e2e39348a44f2015e9966c5adc1b
Dreamwalker <aristojeff@gmail.com>
2018-08-28 23:34:23 +0900

    줄였스

--- app/src/main/java/com/dreamwalker/diabetesreminder/reminder/AlarmScheduler.java
+++ app/src/main/java/com/dreamwalker/diabetesreminder/reminder/AlarmScheduler.java
@@ -20,20 +20,12 @@ public class AlarmScheduler {
         AlarmManager manager = AlarmManagerProvider.getAlarmManager(context);
 
         PendingIntent operation = ReminderAlarmService.getReminderPendingIntent(context, reminderTask);
-
-
         if (Build.VERSION.SDK_INT >= 23) {
-
             manager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTime, operation);
-
         } else if (Build.VERSION.SDK_INT >= 19) {
-
             manager.setExact(AlarmManager.RTC_WAKEUP, alarmTime, operation);
-
         } else {
-
             manager.set(AlarmManager.RTC_WAKEUP, alarmTime, operation);
-
         }
     }
 
