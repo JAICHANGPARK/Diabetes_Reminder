c3f6ef790b9cdc0977e747075d7a0e0ebf01636d
Dreamwalker <aristojeff@gmail.com>
2018-08-30 18:10:32 +0900

    프로바이더 확인 앱 수정

--- provierchecker/src/main/java/com/dreamwalker/provierchecker/HomeActivity.java
+++ provierchecker/src/main/java/com/dreamwalker/provierchecker/HomeActivity.java
@@ -1,13 +1,47 @@
 package com.dreamwalker.provierchecker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.e(TAG, "onCreate: " + " Hello ");
+
+        Uri uri = Uri.parse("com.dreamwalker.diabetesreminder");
+        String[] projection = {
+                AlarmReminderContract.AlarmReminderEntry._ID,
+                AlarmReminderContract.AlarmReminderEntry.KEY_TITLE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_DATE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_TIME,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_NO,
+                AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_TYPE,
+                AlarmReminderContract.AlarmReminderEntry.KEY_ACTIVE
+
+        };
+
+        Cursor cursor = getContentResolver().query(AlarmReminderContract.AlarmReminderEntry.CONTENT_URI, projection, null,null,null);
+
+        if (cursor != null){
+            while (cursor.moveToNext()){
+                Log.e(TAG, "onCreate: " + cursor.getString(1));
+            }
+            cursor.close();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
